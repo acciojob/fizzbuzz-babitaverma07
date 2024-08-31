63c7cadd194dbfec62b8becc4d9e18a814b73dc4
Babita Verma <147910203+babitaverma07@users.noreply.github.com>
2024-08-31 22:21:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+for (let i = 1; i <= 20; i++) {
+  let output = "";
+  if (i % 3 === 0 && i % 5 === 0) {
+    output = "FizzBuzz";
+  } else if (i % 3 === 0) {
+    output = "Fizz";
+  } else if (i % 5 === 0) {
+    output = "Buzz";
+  } else {
+    output = i.toString();
+  }
+  alert(output);
+}
